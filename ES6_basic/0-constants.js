cd779f8188d8af0d7c7ea5a35a587c82894e48c8
Jordyleigha <jordana@Jordanslaptop>
2024-05-07 21:36:02 -0500

    hopefully passes lint

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,12 +1,16 @@
+// taskFirst.js
+
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
-}  
-export function getLast() {
-return ' is okay';
-  }  
+    const task = 'I prefer "const" when I can.'; // Added quotes around the string
+    return task;
+  }
+  
+  export function getLast() {
+    return ' is okay';
+  }
+  
   export function taskNext() {
-let combination = 'But sometimes let';
-combination += getLast();  
-return combination;
+    let combination = 'But sometimes "let"'; // Added quotes around the string
+    combination += getLast();
+    return combination;
   }
