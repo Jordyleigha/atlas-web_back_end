b746b9ecc7ee297906617a36ebb9ec23167d97b4
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-11 14:39:18 -0600

    strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+""" taking two strings as args and return a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+
+    """function name: concat
+    
+    Args:
+    str1 (string): first string
+    str2 (string): second string
+
+    Returns: string (the concatenated result of str1 and str2)
+    """
+    
+    return str1 + str2
