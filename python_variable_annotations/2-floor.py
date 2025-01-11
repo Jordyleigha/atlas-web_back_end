1ad52f99a31a0ffcc17db7b1e3fcb73a94d3956a
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-11 14:56:37 -0600

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""a type annotated function floor that takes a float arg and returns floor.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+
+    """function floor that takes float arg and returns floor.
+    Args:
+    n (float): parameter of type float
+
+    Returns: int (the floor value of n)
+
+    """
+
+    return math.floor(n)
