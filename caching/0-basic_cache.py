27fbbf3a42f496cf638b7e3e1bc5645cc72244f2
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-16 11:30:45 -0600

    create files

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,10 @@
+#!/bin/usr/env python3
+
+"""create a class that inherits from BaseCaching and is a caching system
+"""
+
+
+class BasicCache:
+    def put(self, key, item):
+        self.capacity =
+        self.cache
