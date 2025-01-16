ee5fe186ea372cd9184ea101ed10d4a40908c886
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-16 14:21:50 -0600

    fix

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -5,6 +5,20 @@
 
 
 class BasicCache:
-    def put(self, key, item):
-        self.capacity =
-        self.cache
+    """BasicCache defines:
+    - constants of caching system
+    - where the data is stored (in BaseCaching dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(sefl):
+        """Initiliaze
+        """
+        self.cache_data = {}
+
+        def print_cache(self):
+            """print the cache
+            """
+            print("Current cache:")
+            for key in sorted(self.cache_data.keys()):
+                print("{}: {}" .format(key, self.cache_data.get(key)))
