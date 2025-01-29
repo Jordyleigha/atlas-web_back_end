1543f3e7eed9767efdc3efd2a45ef541fba472da
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 16:51:04 -0600

    fixing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -22,4 +22,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """
     # Create a regex pattern that matches the fields to be obfuscated
     pattern = r'({}){}=([^{}]*)'.format('|'.join(map(re.escape, fields)), re.escape(separator), re.escape(separator))
-    return re.sub(pattern, lambda m: f"{m.group(1)}{separator}={redaction}", message)
+
+    return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={redaction}", message)
