3e03ca7e600afbac32f46d4bf14f29174bc27fb0
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 14:27:27 -0600

    fixing spacing of fucntion

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -61,8 +61,8 @@ class Server:
         return dataset[start_index:end_index]
 
 
-def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-    """Retrieve a hypermedia representation of a specific page of data.
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieve a hypermedia representation of a specific page of data.
 
     Args:
         page (int): The page number to retrieve (1-indexed). Defaults to 1.
@@ -76,15 +76,15 @@ def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
             - "next_page": The next page number if it exists, otherwise None.
             - "prev_page": The previous page number if exists, otherwise None
             - "total_pages": The total number of pages available."""
-    total_pages = math.ceil(len(self.dataset()) / page_size)
-    data = self.get_page(page, page_size)
-    next_page = page + 1 if page < total_pages else None
-    prev_page = page - 1 if page > 1 else None
-    return {
-        "page_size": page_size,
-        "page": page,
-        "data": data,
-        "next_page": next_page,
-        "prev_page": prev_page,
-        "total_pages": total_pages,
-    }
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        data = self.get_page(page, page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
