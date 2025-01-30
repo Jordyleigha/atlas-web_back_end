05f402f804ad86dfdb2ab4cbf5dfd391ca6e9fbb
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 20:02:59 -0600

    push

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""module for API authentication management"""
+from Flask import request
+from typing import List, TypeVar
+
+User = TypeVar('User ')
+
+
+class Auth:
+    """class to manage API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """method to check if auth is required for a given path
+        Args:
+        path(str): the path to check
+        excluded_paths(List[str]): a lis of paths that do not require auth
+        
+        returns:
+        bool: false (auth logic to be implemented later)"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """method to retrieve the authorization header from the request
+        
+        args:
+        request: the flask request object
+        
+        returns:
+        str: None (logic to be implemented later)"""
+        return None
+    def current_user(self, request=None) -> User:
+        """method to retrieve the current user from the request
+        
+        args:
+        request: the flask request object
+        
+        returns:
+        User: None"""
+        return None
