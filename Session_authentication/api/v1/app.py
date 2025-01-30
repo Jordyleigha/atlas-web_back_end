992e489e826f18e9334afe7dbb5a8ba44011b0fe
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 21:14:33 -0600

    updating files

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -14,14 +14,24 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = getenv("AUTH_TYPE")
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-if auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+
+excluded_paths = [
+    "/api/v1/status/",
+    "/api/v1/unauthorized/",
+    "/api/v1/forbidden/",
+    "/api/v1/auth_session/login/",
+]
 
 
 @app.errorhandler(404)
