ff41c6711152aa3ee550b451bd3a4dc242934742
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 19:27:49 -0600

    push

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """endpoint that raises 401 error"""
+    abort(401)
