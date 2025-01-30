cce7c1c134aecdf76a5164a19c0051e7f372fadc
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 18:26:00 -0600

    idk

--- Basic_authentication/SimpleAPI/api/v1/app.py
+++ Basic_authentication/SimpleAPI/api/v1/app.py
@@ -14,8 +14,8 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
+@app.errorhandler(401)
+def unauthorized(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
