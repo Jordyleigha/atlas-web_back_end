a9b84ed3cc7632ee9ca816499822dd03f2381399
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 20:53:26 -0600

    fix

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 
@@ -26,36 +26,35 @@ else:
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """unauthorized handler"""
+    """Unauthorized request error handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """forbidden handler"""
+    """Forbidden request error handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request():
-    """filter each request before process handler"""
+    """Filters each request before process handler"""
     if auth is None:
         return
-    excluded_path = [
+    excluded_paths = [
         "/api/v1/status/",
         "/api/v1/unauthorized/",
         "/api/v1/forbidden/"
         ]
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if aith.authorization_header(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
