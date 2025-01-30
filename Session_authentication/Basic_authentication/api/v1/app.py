4777bcfbcc3f89f24be86070f9ae5e62d5c1668b
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-01-29 21:07:58 -0600

    updating files

--- Session_authentication/Basic_authentication/api/v1/app.py
+++ Session_authentication/Basic_authentication/api/v1/app.py
@@ -47,17 +47,14 @@ def before_request():
     """Filters each request before process handler"""
     if auth is None:
         return
-    excluded_paths = [
-        "/api/v1/status/",
-        "/api/v1/unauthorized/",
-        "/api/v1/forbidden/"
-        ]
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+        auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
