f803951e99ffc6ee161ac33e74eb4b38c2be3096
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-02-11 16:54:52 -0600

    update file

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -71,3 +71,19 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         # retrieve the value as an integer
         return self.get(key, fn=int)
+
+
+def replay(method: Callable) -> None:
+    # get input and output keys
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+    # retrieve the inputs and outputs from redis
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+    # get the number of calls
+    call_count = len(inputs)
+    # print the replay information
+    print(f"{method.__qualname__} was called {call_count} times:")
+    # loop through inputs and outputs and print them
+    for input_data, output_data in zip(inputs, outputs):
+        print(f"{method.__qualname__}(*{eval(input_data.decode('utf-8'))}) -> {output_data.decode('utf-8')}")
