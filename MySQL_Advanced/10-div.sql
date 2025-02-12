c6acda009aa2c34a41d9f2a36feff24846db48e0
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-02-12 17:12:51 -0600

    update

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,20 @@
+-- creates a function SafeDiv that divides (and returns) the first
+-- by the second number or returns 0 if the second number is equal to 0
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT) 
+RETURNS INT
+BEGIN
+    DECLARE result INT;
+
+    -- Check if b is 0
+    IF b = 0 THEN
+        SET result = 0;  -- Return 0 if b is 0
+    ELSE
+        SET result = a / b;  -- Perform division if b is not 0
+    END IF;
+
+    RETURN result;  -- Return the result
+END //
+
+DELIMITER ;
