9457d2705ae3ef20725e9c6f72c1e3ff74083a30
Jordan Arguelles <Jordan.arguelles.ja@gmail.com>
2025-02-19 20:31:33 -0600

    update file

--- MySQL_Advanced/4-init.sql
+++ MySQL_Advanced/4-init.sql
@@ -1,13 +1,11 @@
 -- Initial
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
-
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
+delimiter $$
+CREATE TRIGGER itemorder BEFORE INSERT ON orders
+    FOR EACH ROW
+    BEGIN
+        UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+    END;
+$$
+delimiter ;
